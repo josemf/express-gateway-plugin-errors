708efd8beac4894be92eed319af21578b5111ed8
José Mata Fernandes <josemataf@gmail.com>
2019-09-24 15:52:27 +0100

    Plugin policy code for json error formating, and error massaging managing

---
+++ policies/errors.js
@@ -0,0 +1,81 @@
+const axios = require('axios');
+const _ = require("lodash");
+
+
+const OutputJsonSender = require('./output_json_sender');
+
+module.exports = {
+    name: 'errors',
+    schema: {
+        $id: 'http://express-gateway.io/schemas/policies/errors.json', 
+        type: 'object',
+        properties: {
+
+            template: {
+                type: "string",                
+                default: '{ "status": "$status", "message": "$message", "code": "$code", "exception": "$exception" }'
+            },
+
+            defaults: {
+                type: "object",
+                default: { status: "NOK", code: "server_error", message: "Server Error" }
+            },
+
+            debugFields: {
+                type: "array",
+                items: {
+                    type: "string"
+                }
+            },
+
+            messageField: {
+                type: "string"
+            },
+            
+            filters: {
+                type: "array",
+                items: {
+                    type: "object",
+                    properties: {
+                        regex: {
+                            type: "string"
+                        } 
+                    }   
+                }  
+            },
+ 
+            messages: {   
+                type: "object"                
+            },
+ 
+            restrictErrors: {  
+                type: "boolean", 
+                default: true
+            }
+        }
+    },
+    policy: (actionParams) => { 
+
+        const output = new OutputJsonSender(actionParams);
+
+        return (req, res, next) => {
+
+            output.response(res); 
+
+            // Monkey patch res.send
+            
+            res.send = new Proxy(res.send, {
+                apply: (fn, thisArg, args) => {
+                    
+                    if(res.statusCode === 200) {
+                        return fn.apply(res, args);
+                    }
+                    
+                    return output.return(args[0]); 
+                } 
+            });    
+
+            next();  
+        }; 
+    }
+};
